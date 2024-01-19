18404cf512f76549f9995c8a56e958feddce1d13
Davydov Egor <58881280+Butters7@users.noreply.github.com>
2024-01-19 04:00:54 +0300

    FinishedHW

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"notes/controller/stdhttp"
+	"notes/gates/storage"
+	"notes/gates/storage/mp"
+	"notes/gates/storage/list"
+)
+
+func main() {
+	var st storage.Storage
+	// Определение флагов
+	flagList := flag.Bool("l", false, "Use list storage")
+	flagMap := flag.Bool("m", false, "Use map storage")
+
+	// Разбор аргументов командной строки
+	flag.Parse()
+
+	// Проверка, что установлен один из флагов
+	if !(*flagList || *flagMap) || (*flagList && *flagMap) {
+		log.Println("Error: you must add flag -l to use list storage or flag -m to use map storage")
+		flag.PrintDefaults()
+		return
+	}
+
+	// Если есть другие аргументы, выдаем ошибку
+	if len(flag.Args()) > 0 {
+		log.Println("Error: bad arguments")
+		flag.PrintDefaults()
+		return
+	}
+
+	// Проверка значений флагов
+	if *flagList {
+		st = list.NewList()
+	}
+	if *flagMap {
+		st = mp.NewMap()
+	}
+	hs := stdhttp.NewController(":4040", st)
+	hs.Start()
+
+}
